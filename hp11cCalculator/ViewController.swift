8deeb930c9e5d45d02518c28b38e18d8985da3ec
Michael Coomey <mcoomey@gmail.com>
2018-01-29 20:32:48 -0500

    fixed minor UI errors

--- hp11cCalculator/ViewController.swift
+++ hp11cCalculator/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    let impact = UIImpactFeedbackGenerator()
+    let selection = UISelectionFeedbackGenerator()
+    let notification = UINotificationFeedbackGenerator()
+    
+    
+    
+    @IBAction func clearPressed(_ sender: UIButton) {
+        impact.impactOccurred()
+        display.text = "0.0"
+    }
+    
+    
+    @IBAction func onPressed(_ sender: UIButton) {
+        impact.impactOccurred()
+//        selection.selectionChanged()
+//        notification.notificationOccurred(.error)
+        display.text = "01234567890123456789"
 
+    
+    }
+    
 }
 
